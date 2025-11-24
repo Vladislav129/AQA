9e7cb6ac456a01a0b8912e38056313f61f11010a
nikmi <nikmi8998@gmail.com>
2025-11-24 18:05:39 +0400

    new lesson

---
+++ src/Lesson3/Park.java
@@ -0,0 +1,62 @@
+package Lesson3;
+
+public class Park {
+    public static class InfoAttraction {
+        String name;
+        String location;
+        int minPeople;
+        int maxPeople;
+        int acceptebleAge;
+        String workTime;
+        double price;
+
+        public InfoAttraction(String name, String location, int minPeople, int maxPeople, int acceptebleAge, String workTime, double price) {
+            this.name = name;
+            this.location = location;
+            this.minPeople = minPeople;
+            this.maxPeople = maxPeople;
+            this.acceptebleAge = acceptebleAge;
+            this.workTime = workTime;
+            this.price = price;
+        }
+
+        public void info() {
+            System.out.println("Название аттракциона: " + name);
+            System.out.println("Местополажение аттракциона: " + location);
+            System.out.println("Минимальное количество людей: " + minPeople);
+            System.out.println("Максимальное количество людей: " + maxPeople);
+            System.out.println("Допустимый возвраст: " + acceptebleAge);
+            System.out.println("Время работы аттракциона: " + workTime);
+            System.out.println("Цена: " + price);
+        }
+    }
+
+    InfoAttraction infoAttraction;
+    String namePark;
+    String locationPark;
+    String workTimePark;
+
+    public Park(String namePark, String locationPark, String workTimePark, InfoAttraction infoAttraction) {
+        this.namePark = namePark;
+        this.locationPark = locationPark;
+        this.workTimePark = workTimePark;
+        this.infoAttraction = infoAttraction;
+    }
+
+    public void printPark() {
+        System.out.println("Название парка: " + namePark);
+        System.out.println("Адрес парка: " + locationPark);
+        System.out.println("Время работы парка: " + workTimePark);
+        System.out.println();
+        infoAttraction.info();
+    }
+    public static void infoPark() {
+        InfoAttraction attraction = new InfoAttraction(
+                "Карусель", "Зона С", 2, 15, 12, "10:00 - 20:00", 200
+        );
+
+        Park park1 = new Park("Диснейленд", "Лондон", "9:00 - 24:00", attraction);
+
+        park1.printPark();
+    }
+}
---
+++ src/Lesson3/Product.java
@@ -0,0 +1,37 @@
+package Lesson3;
+
+public class Product {
+    String name;
+    String manufacturerDate;
+    String manufacturer;
+    String countryOfOrigin;
+    double price;
+    boolean reservation;
+    public Product(String name, String manufacturerDate, String manufacturer, String countryOfOrigin, double price, boolean reservation) {
+        this.name = name;
+        this.manufacturerDate = manufacturerDate;
+        this.manufacturer = manufacturer;
+        this.countryOfOrigin = countryOfOrigin;
+        this.price = price;
+        this.reservation = reservation;
+    }
+    public void info (){
+        System.out.println("Имя: " + name);
+        System.out.println("Дата производств: " + manufacturerDate);
+        System.out.println("Производитель: " + manufacturer);
+        System.out.println("Страна происхождения: " + countryOfOrigin);
+        System.out.println("Цена: " + price);
+        System.out.println("Бронирование: " + reservation);
+    }
+    public static void productArray (){
+        Product[] productsArray = new Product[5];
+        productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 1299.99, true);
+        productsArray[1] = new Product("Iphone 17", "10.10.2025", "Apple Inc.", "India", 799, false);
+        productsArray[2] = new Product("Iphone 17 Pro", "01.10.2025", "Apple Inc.", "USA", 1099, false);
+        productsArray[3] = new Product("Pixel 10", "14.09.2025", "Google LLC", "China", 799, true);
+        productsArray[4] = new Product("Huawei Mate XT", "18.02.2025", "Huawei Technologies Co. Ltd", "China", 2835, false);
+        for (Product product : productsArray) {
+            product.info();
+        }
+    }
+}
