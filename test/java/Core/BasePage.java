dd5a17a1487b1796a26b5db2e0bcabd3e629f0e1
nikmi <nikmi8998@gmail.com>
2025-12-15 21:44:35 +0400

    Lesson 9

--- test/java/Core/BasePage.java
+++ test/java/Core/BasePage.java
@@ -1,10 +1,6 @@
 package Core;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 
 public class BasePage {
--- test/java/Core/SettingTest.java
+++ test/java/Core/SettingTest.java
@@ -3,12 +3,8 @@ package Core;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
 
 import java.time.Duration;
 
